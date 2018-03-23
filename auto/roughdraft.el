321cca7f3d95134ad127524a07569e958324b812
Berthold Lorke <berthold@lorke.de>
2018-03-23 16:02:52 +0100

    Almost done mofo

--- auto/roughdraft.el
+++ auto/roughdraft.el
@@ -1,15 +1,48 @@
 (TeX-add-style-hook
  "roughdraft"
  (lambda ()
-   (LaTeX-add-bibitems
-    "Aud03"
-    "Bos13"
-    "DK15"
-    "FH91"
-    "Fis14"
-    "Kle93"
-    "Kra85"
-    "Rab30"
-    "Stu08"))
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "zusammen"
+    "introduction"
+    "prework"
+    "reynolds"
+    "prozess"
+    "discussion"
+    "article"
+    "art10"
+    "graphicx"
+    "amssymb"
+    "amsmath"
+    "amsthm"
+    "cite"
+    "mathtools"
+    "enumerate"
+    "perpage"
+    "blindtext")
+   (LaTeX-add-labels
+    "pw")
+   (LaTeX-add-bibliographies)
+   (LaTeX-add-amsthm-newtheorems
+    "theorem"
+    "definition"
+    "lemma"
+    "proposition"
+    "corollary"
+    "remark"
+    "cremark"
+    "example"
+    "dexample")
+   (LaTeX-add-amsthm-newtheoremstyles
+    "prrt"))
  :latex)
 
